c883f18859593fac5f93b48e02f671c3bb62652e
fernando.gonzalez <fernando.stoiano@gmail.com>
2019-07-20 13:00:49 -0300

    First commit

---
+++ src/main/java/com/gympass/speedland/SpeedlandApplication.java
@@ -0,0 +1,13 @@
+package com.gympass.speedland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedlandApplication.class, args);
+	}
+
+}
