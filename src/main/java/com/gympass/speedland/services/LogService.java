248c5de70edc4231c8b94bad11447d45598d89f4
fernando.gonzalez <fernando.stoiano@gmail.com>
2019-07-20 23:35:56 -0300

    Implemented speed file log read line

--- src/main/java/com/gympass/speedland/services/LogService.java
+++ src/main/java/com/gympass/speedland/services/LogService.java
@@ -3,24 +3,38 @@ package com.gympass.speedland.services;
 import com.gympass.speedland.converter.LineDtoConverter;
 import com.gympass.speedland.dto.LineDto;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class LogService {
 
-    public void readLog() {
+    public void readLog() throws IOException {
 
-        String line = "23:49:08.277      038 – F.MASSA                           1     1:02.852                        44,275";
+        Path path = Paths.get("/Users/fernandostoianogonzalez/dev/gympass/speedland/speed.txt");
 
-        LineDtoConverter lineDtoConverter = new LineDtoConverter();
+        List<String> lines = Files.readAllLines(path);
 
-        LineDto lineDto = lineDtoConverter.apply(line);
+        //String line = "23:49:08.277      038 – F.MASSA                           1     1:02.852                        44,275";
 
-        System.out.println(lineDto);
+        for(String line : lines) {
+            LineDtoConverter lineDtoConverter = new LineDtoConverter();
+            LineDto lineDto = lineDtoConverter.apply(line);
+            System.out.println(lineDto);
+        }
     }
 
     public static void main(String ...args) {
 
         LogService service = new LogService();
 
-        service.readLog();
+        try {
+            service.readLog();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
